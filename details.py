a4cd31323a50c4a1f6cb4b75bd2db326af422c61
arzaan <xdarzanplug@gmail.com>
2024-01-28 19:14:41 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 4942197
+api_hash = "13248a2c551b73193969b42194023635"
+bot_token = "6735364074:AAEBEqqdpTciTAeanM3B4_yU9uvb3PIXbqA"
+auth_users = [5892781710]
+sudo_user = 5892781710
+log_channel = -1002073188657
+txt_channel = -1002001856626
