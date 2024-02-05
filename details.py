dbb7fe82d32d76ce4a4e1ac24a0ed485950de243
arzaan <xdarzanplug@gmail.com>
2024-02-05 12:41:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 4942197
 api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6735331136:AAE6V41FSGrCLCflNy0aUM9kJuxMvuahbS0"
+bot_token = "6913163552:AAHy8Tmi-HYELZkViq0rUVLQ7cTWjYJoiMk"
 auth_users = [5892781710]
 sudo_user = 5892781710
 log_channel = -1002073188657
